b7b9b52c076afead53ed12824d9a3d6d30f0527f
Nishant Sharma <nishantsharma2633@gmail.com>
2023-05-12 19:37:35 -0400

    adds Linktree link to music page

--- pages/music.js
+++ pages/music.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Music() {
   return (
-    <div className="flex min-h-screen bg-black text-2xl font-bold justify-center items-center text-white">
+    <div className="flex flex-col min-h-screen bg-black text-2xl font-bold justify-center items-center text-white">
       <p>On the Way :)</p>
+      <p>
+        Till then, chech out my music{" "}
+        <a href="https://linktr.ee/nisharrk" className="text-teal-500">
+          here.
+        </a>
+      </p>
     </div>
   );
 }
