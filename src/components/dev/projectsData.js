4168b5806e98ec271ba2e926912bbead663f5e45
Nishant Sharma <nishantsharma2633@gmail.com>
2024-04-26 13:25:28 -0400

    adds travel log project

--- src/components/dev/projectsData.js
+++ src/components/dev/projectsData.js
@@ -2,6 +2,7 @@ import Budget from "../../../public/budget.png";
 import Iamlazy from "../../../public/iamlazy.png";
 import Museum from "../../../public/museum.png";
 import Todo from "../../../public/todo.png";
+import Travellog from "../../../public/travellog.png";
 
 const projects = [
   {
@@ -12,12 +13,19 @@ const projects = [
     tech: ["Node", "Express", "MongoDB", "HTML", "CSS"],
   },
   {
-    name: "Metropolitan Museum of Art",
+    name: "Travel Log",
     description:
-      "An artworks database site using the Metropolitan Museum of Art Collection API",
-    url: "https://web422-assign6-phi.vercel.app/",
-    image: Museum,
-    tech: ["NextJS", "JWT", "Jotai", "SWR", "REST API", "Bootstrap"],
+      "A full stack application for to log the places I have travelled",
+    url: "https://travel-log-rosy.vercel.app/",
+    image: Travellog,
+    tech: [
+      "NextJS",
+      "TypeScript",
+      "MapBox",
+      "Tailwindcss",
+      "React-Leaflet",
+      "Daisy-UI",
+    ],
   },
   {
     name: "To-Do App",
@@ -25,6 +33,14 @@ const projects = [
     url: "https://to-do-coral-nine.vercel.app/",
     image: Todo,
     tech: ["React", "Chakra-UI"],
+  },
+  {
+    name: "Metropolitan Museum of Art",
+    description:
+      "An artworks database site using the Metropolitan Museum of Art Collection API",
+    url: "https://web422-assign6-phi.vercel.app/",
+    image: Museum,
+    tech: ["NextJS", "JWT", "Jotai", "SWR", "REST API", "Bootstrap"],
   },
   {
     name: "Budget Tracker",
